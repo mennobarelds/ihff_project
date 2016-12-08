abb6a1b89039d77330555b7390704922542e0b02
MSpaans4 <mick-spaans@hotmail.com>
2016-12-08 19:28:33 +0100

    iHff project

---
+++ ihff project/ihff project/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ihff_project.MvcApplication" Language="C#" %>
---
+++ ihff project/ihff project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ihff_project.Startup))]
+namespace ihff_project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
