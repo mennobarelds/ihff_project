9f2207708ba9832d9988a65e7033ca3dd34c2dba
MSpaans4 <mick-spaans@hotmail.com>
2016-12-09 13:55:14 +0100

    interface

--- ihff project/ihff project/Repository/IProductRepository.cs
+++ ihff project/ihff project/Repository/IProductRepository.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ihff_project.Models;
 
 namespace ihff_project.Repository
 {
     interface IProductRepository
     {
+        IEnumerable<Films> GetAllFilms();
+        IEnumerable<Voorstellingen> GetAllVoorstellingenFilm(int filmId);
+        Producten GetVoorstellingen(int productId);
+        Films GetFilm(int filmId);
+        Voorstellingen GetVoorstelling(int productId);
+        void EditFilm(Films film);
+
     }
 }
