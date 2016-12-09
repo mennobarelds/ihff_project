7c8e4ad9d82147c3e8cdf9e37e79c1c4d64fbbaf
MSpaans4 <mick-spaans@hotmail.com>
2016-12-09 10:23:50 +0100

    DbEntities
    Via entitie framework alle models aangemaakt

---
+++ ihff project/ihff project/Repository/DbFilmRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ihff_project.Models;
+
+namespace ihff_project.Repository
+{
+    public class DbFilmRepository : IFilmRepository
+    {
+        private DatabaseContext ctx = new DatabaseContext();
+
+        public IEnumerable<Film> GetAllFilms()
+        {
+            IEnumerable<Film> allFilms = ctx.Films;
+            return allFilms;
+        }
+
+        public Film GetFilm(int filmId)
+        {
+            return ctx.Films.SingleOrDefault(c => c.Film_ID == filmId);
+        }
+
+        public void EditFilm(Film film)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ihff project/ihff project/Repository/IFilmRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ihff_project.Models;
+
+namespace ihff_project.Repository
+{
+    interface IFilmRepository
+    {
+        IEnumerable<Film> GetAllFilms();
+        Film GetFilm(int filmId);
+        void EditFilm(Film film);
+    }
+}
