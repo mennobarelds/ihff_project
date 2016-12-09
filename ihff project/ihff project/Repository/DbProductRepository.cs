671770e996f1fbf4a6cbcbcadf669c8bc9295eb4
MSpaans4 <mick-spaans@hotmail.com>
2016-12-09 13:46:32 +0100

    Menno hoi

--- ihff project/ihff project/Repository/DbProductRepository.cs
+++ ihff project/ihff project/Repository/DbProductRepository.cs
@@ -8,36 +8,36 @@ namespace ihff_project.Repository
 {
     public class DbProductRepository : IProductRepository
     {
-        private iHFF1617S_B3Entities ctx = new iHFF1617S_B3Entities();
+        private iHFF1617S_B3 ctx = new iHFF1617S_B3();
 
-        public IEnumerable<Film> GetAllFilms()
+        public IEnumerable<Films> GetAllFilms()
         {
-            IEnumerable<Film> allFilms = ctx.Films;
+            IEnumerable<Films> allFilms = ctx.Films;
             return allFilms;
         }
 
         public IEnumerable<Voorstellingen> GetAllVoorstellingenFilm(int filmId)
         {
-            IEnumerable<Voorstellingen> allVoorstellingen = ctx.Voorstellingens.Where(c => c.Film_ID == filmId);
+            IEnumerable<Voorstellingen> allVoorstellingen = ctx.Voorstellingen.Where(c => c.Film_ID == filmId);
             return allVoorstellingen;
         }
 
         public Producten GetVoorstellingen(int productId)
         {
-            return ctx.Productens.SingleOrDefault(c => c.Product_ID == productId);
+            return ctx.Producten.SingleOrDefault(c => c.Product_ID == productId);
         }
 
-        public Film GetFilm(int filmId)
+        public Films GetFilm(int filmId)
         {
             return ctx.Films.SingleOrDefault(c => c.Film_ID == filmId);
         }
 
         public Voorstellingen GetVoorstelling(int productId)
         {
-            return ctx.Voorstellingens.SingleOrDefault(c => c.Product_ID == productId);
+            return ctx.Voorstellingen.SingleOrDefault(c => c.Product_ID == productId);
         }
 
-        public void EditFilm(Film film)
+        public void EditFilm(Films film)
         {
             throw new NotImplementedException();
         }
